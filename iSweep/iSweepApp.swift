0f64dcd850b1bcfdebdf7681bc48eeb73241a7fe
vital kyungu <vitalkabange77@gmail.com>
2025-04-07 18:15:40 -0700

    Initial Commit

---
+++ iSweep/iSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSweepApp.swift
+//  iSweep
+//
+//  Created by Vital Kyungu on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct iSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
